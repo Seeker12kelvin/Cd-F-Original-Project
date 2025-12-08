008baeb5e0c87bfe43e48ebd5c82345bb05ed056
C-G <kelvindonye@gmail.com>
2025-12-08 01:03:16 +0100

    Worked on the landing Page.

--- client/src/Pages/LandingPage/landingPage.jsx
+++ client/src/Pages/LandingPage/landingPage.jsx
@@ -48,6 +48,23 @@ const LandingPage = () => {
     }
   ]
 
+  const sectionData2 = [
+    {
+      style: 'pr-15 border-r-[0.09375rem] border-white',
+      number: '7.4%',
+      text: 'Property Return Rate'
+    },
+    {
+      style: 'pr-15 border-r-[0.09375rem] border-white',
+      number: '3,856',
+      text: 'Property in Sell & Rent',
+    },
+    {
+      number: '2,540',
+      text: 'Daily Completed Transactions',
+    }
+  ]
+
   return (
     <div>
       <section className={`${styles['onLoad-animation']} w-full h-screen bg-linear-to-b from-[#E0DEF7] to-[#e0def700] pl-10 flex justify-center items-center gap-6 `}>
@@ -128,14 +145,14 @@ const LandingPage = () => {
           </div>
         </div>
 
-        <div className={`${styles['no-scrollbar']} flex justify-start gap-6 w-full flex-wrap h-200 overflow-scroll mb-10`}>
+        <div className={`${styles['no-scrollbar']} flex justify-start gap-6 w-full flex-wrap h-200 overflow-hidden mb-10`}>
           {filteredProperties.map(data => {
               return (
                 <Link key={data.id} to={`/rent/${data.id}`}>
                   <div
                     className={`${styles['onLoad-animation']} bg-white w-[330px] h-fit scale-[0.9] relative shadow-xl shadow-[#00000010] rounded-2xl border-[#D6DDEB] border-2 flex flex-col`}>
           
-                    <img className='object-contain rounded-xl rounded-b-none z-3 bg-[white] h-fit' src={data.imgUrl} alt={data.name}/>
+                    <img className='object-cover rounded-xl rounded-b-none z-3 bg-[white] h-47' src={data.imgUrl} alt={data.name}/>
 
                     <div className='bg-white h-full w-full z-3 flex rounded-b-2xl gap-4 flex-col p-6'>
           
@@ -148,7 +165,6 @@ const LandingPage = () => {
                         </div>
                         <div className='border-2 border-[#F0EFFB] flex items-center p-1.5 -mt-10 rounded-[50%] w-fit'>    
                           <FavoriteButton
-                            className=''
                             isFavorite={favoriteDetails.some(item => item.id === data.id)}
                             onToggle={() => {
                               if (favoriteDetails.some(item => item.id === data.id)) {
@@ -193,27 +209,50 @@ const LandingPage = () => {
         <Link to={'/rent'} className='bg-[#100A55] p-2 text-white rounded-lg pl-6 pr-6'>Browse more properties</Link>
       </section>
 
-      <section className='bg-[#100A55] pl-40 pt-20 pb-20 w-full'>
-        <div className='flex justify-between items-center w-[85%] mb-15'>
-          <h2 className='text-[2.5rem] font-bold text-white'>We make it easy for <br/> <span className='text-[#7065F0]'>tenants</span> and <span className='text-[#7065F0]'>landlords.</span></h2>
-          <p className='text-[1rem] font-normal text-[#ffffffca]'>Whether it’s selling your current home, getting <br/> financing, or buying a new home, we make it easy  and <br/> efficient. The best part? you’ll save a bunch of money <br/> and time with our services.</p>
-        </div>
-        
-        <div className={`flex gap-6 w-full overflow-scroll ${styles['no-scrollbar']}`}>
-          {sectionData.map(data => {
-            return(
-              <div className={`${data.style} flex gap-4 h-48 items-center justify-between`}>
-                <div className={`${data.iconStyle} -mt-15`}>{data.icon}</div>
-                <div>
-                  <h3 className='font-bold text-2xl mb-3'>{data.topic}</h3>
+      <section className='bg-[#100A55] pt-30 pb-30 w-full flex flex-col gap-15'>
+        <div className='pl-40 pb-20 border-b-[#312F4B] border-b-2'>
+          <div className='flex justify-between items-center w-[85%] mb-15'>
+            <h2 className='text-[2.5rem] font-bold text-white'>We make it easy for <br/> <span className='text-[#7065F0]'>tenants</span> and <span className='text-[#7065F0]'>landlords.</span></h2>
+            <p className='text-[1rem] font-normal text-[#ffffffca]'>Whether it’s selling your current home, getting <br/> financing, or buying a new home, we make it easy  and <br/> efficient. The best part? you’ll save a bunch of money <br/> and time with our services.</p>
+          </div>
+          
+          <div className={`flex gap-6 w-full overflow-scroll ${styles['no-scrollbar']}`}>
+            {sectionData.map(data => {
+              return(
+                <div className={`${data.style} flex gap-4 h-48 items-center justify-between`}>
+                  <div className={`${data.iconStyle} -mt-15`}>{data.icon}</div>
+                  <div>
+                    <h3 className='font-bold text-2xl mb-3'>{data.topic}</h3>
 
-                  <p className='text-[1rem] font-normal w-90 opacity-70'>{data.description}</p>
+                    <p className='text-[1rem] font-normal w-90 opacity-70'>{data.description}</p>
+                  </div>
                 </div>
-              </div>
+              )
+            })}
+          </div>
+        </div>
+
+        <ul className='flex gap-15 items-center self-center'>
+          {sectionData2.map(data => {
+            return(
+              <li className={`text-[2.5rem] text-white font-bold flex flex-col items-center ${data.style}`}>{data.number}<span className='text-[1rem] font-normal opacity-70'>{data.text}</span></li>
             )
           })}
-        </div>
+        </ul>
         
+      </section>
+
+      <section className='flex flex-col items-center justify-center gap-4 bg-linear-to-b from-[#F7F7FD] to-[#F7F7FD] p-50 pt-20 pb-20'>
+        <span className='text-[#7065F0] text-2xl font-bold'>No Spam Promise</span>
+        <h2 className='text-[#000929] font-bold text-[2.5rem] '>Are you a landlord?</h2>
+        <p className='text-[1rem] font-normal opacity-50'>Discover ways to increase your home's value and  get listed. No Spam.</p>
+
+        <div className='flex items-center gap-4 w-[50%] bg-[white] rounded-lg p-4'>
+          <input className='w-full outline-none' placeholder='Enter your email address' />
+          <button className='bg-[#7065F0] text-white rounded-lg p-3 pl-10 pr-10 w-fit'>Submit</button>
+        </div>
+
+        <p className='text-sm font-medium'>Join <span className='text-[#7065F0]'>10,000+</span> other landlords in our estatery community.</p>
       </section>
     </div>
   )
